86db5fa7aa72cceddeb722a7476ca059311d4fd1
Luciano Mendonca <luciano.s.mend@gmail.com>
2024-08-12 22:03:55 -0300

    estrutura inicial

---
+++ src/main/java/br/luciano/rest/core/BaseTest.java
@@ -0,0 +1,28 @@
+package br.luciano.rest.core;
+
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+
+public class BaseTest implements Constantes {
+
+	@BeforeClass
+	public static void setup() {
+		RestAssured.basePath = APP_BASE_URL;
+		RestAssured.port = APP_PORT;
+		RestAssured.basePath = APP_BASE_PATH;
+		
+		RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+		requestSpecBuilder.setContentType(APP_CONTENT_TYPE);
+		RestAssured.requestSpecification = requestSpecBuilder.build();
+		
+		ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+		responseSpecBuilder.expectResponseTime(Matchers.lessThan(MAX_TIMEOUT));
+		RestAssured.responseSpecification = responseSpecBuilder.build();
+		
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+	}
+}
