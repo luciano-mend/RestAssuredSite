9f112c3851250b76b5456ab61d536fbdfa5db8db
Luciano Mendonca <luciano.s.mend@gmail.com>
2024-08-20 22:00:59 -0300

    Deve incluir conta com sucesso

--- src/main/java/br/luciano/rest/tests/SiteTest.java
+++ src/main/java/br/luciano/rest/tests/SiteTest.java
@@ -2,6 +2,9 @@ package br.luciano.rest.tests;
 
 import static io.restassured.RestAssured.given;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.Test;
 
 import br.luciano.rest.core.BaseTest;
@@ -17,5 +20,31 @@ public class SiteTest extends BaseTest{
 		.then()
 			.statusCode(401)
 		;
+	}
+	
+	@Test
+	public void deveIncluriContaComSucesso() {
+		Map<String, String> login = new HashMap<>();
+		login.put("email", "luciano@email.com");
+		login.put("senha", "123456");
+		
+		String token = given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token")
+		;
+		
+		given()
+			.header("Authorization", "JWT " + token)
+			.body("{ \"nome\": \"conta nova\" }")
+		.when()
+			.post("/contas")
+		.then()
+			.statusCode(201)
+		;
+		
 	}
 }
